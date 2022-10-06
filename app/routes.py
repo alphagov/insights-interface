9e8ead602fb6fd865fccf13d41e0331b62a31451
James Marvin <james.marvin@digital.cabinet-office.gov.uk>
2022-10-06 13:30:50 +0100

    Changes to enable file listing

--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,42 @@
 import json
 import os
+import markdown
 
 from flask import render_template
 from werkzeug.exceptions import NotFound
 
 from app import app
 
+ROOT_DIR = os.getcwd()
+
+class page:
+    def __init__(self, dir_, file):
+        self.path = os.path.join(dir_, file)
+        self.name = os.path.splitext(file)[0]
+        self.ext = os.path.splitext(file)[1]
+        with open(self.path) as f: 
+                self.raw_text = f.read()
+
+        md = markdown.Markdown(extensions=['tables', 'footnotes', 'toc'])
+        self.html = md.convert(self.raw_text)
+        self.toc = md.toc 
+        self.toc_tokens = md.toc_tokens
+
+def _get_pages(directory):
+    pages = []
+    for dir_, _, files in os.walk(directory): 
+        for file in files: 
+            pages.append(page(dir_, file))
+    return pages
+
 
 @app.route("/")
 def index():
     components = os.listdir("govuk_components")
     components.sort()
+    content_dir = f'{ROOT_DIR}/app/content'
+    print(content_dir)
+    print([page_.path for page_ in _get_pages(content_dir)])
 
     return render_template("index.html", components=components)
 
